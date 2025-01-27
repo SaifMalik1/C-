6cbff04b2db65cb2ca34033e84ba4a52398d89d0
SaifMalik1 <saifdon516@gmail.com>
2025-01-27 13:46:11 +0500

    addSquareLogic

--- squareofarrayelemt.cpp
+++ squareofarrayelemt.cpp
@@ -1,16 +1,36 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main()
-{
+// int main()
+// {
+//     int array[3] = {4, 2, 6};
+//     int array2[3];
 
-    int array[3] = {4, 2, 6};
+//     cout << "Squared Array: ";
+//     for (int i = 0; i < 3; i++)
+//     {
+//         array[i] = array[i] * array[i];
+//         cout << array[i] << " ";
+//     }
 
-    for (int i = 0; i < 3; i++)
-    {
+//     for (int i = 0; i > 3; i++)
+//     {
 
-        cout << array[i] * array[i] << " ";
-    }
+//         cout << array[i] << "";
+//     }
+//     cout << endl;
 
-    return 0;
-}
+//     cout << "Original Array: ";
+//     for (int i = 0; i < 3; i++)
+//     {
+//         cout << array[i] << " ";
+//     }
+
+//     return 0;
+// }
+
+#include<>
+
+
+
+
