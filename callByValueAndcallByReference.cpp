e64d2d791c2f93ccab35f4b06caa7f93d38b6941
SaifMalik1 <saifdon516@gmail.com>
2025-01-27 19:01:50 +0500

    add

---
+++ callByValueAndcallByReference.cpp
@@ -0,0 +1,38 @@
+  //Call By Value
+  
+// #include <iostream>
+// using namespace std;
+
+// void modifyValue(int a)
+// {
+//     a = a * a;
+//     cout << a << endl;
+// }
+
+// int main()
+// {
+//     int a = 5;
+//     modifyValue(a);
+//     cout << a << endl;
+//     return 0;
+// }
+
+
+// Call By Reference
+
+// #include <iostream>
+// using namespace std;
+
+// void modifyValue(int a)
+// {
+//     a = a * a;
+//     cout << a << endl;
+// }
+
+// int main()
+// {
+//     int a = 5;
+//     modifyValue(a);
+//     cout << a << endl;
+//     return 0;
+// }
--- squareofarrayelemt.cpp
+++ squareofarrayelemt.cpp
@@ -29,8 +29,27 @@
 //     return 0;
 // }
 
-#include<>
+// #include <iostream>
+// using namespace std;
 
+// void square(int array[], int size)
+// {
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = array[i] * array[i];
+//     }
+// }
 
+// int main()
+// {
+//     int array[3] = {4, 2, 6};
 
+//     square(array, 3);
 
+//     for (int i = 0; i < 3; i++)
+//     {
+//         cout << array[i] << " ";
+//     }
+
+//     return 0;
+// }
